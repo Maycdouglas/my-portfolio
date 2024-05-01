1cf0cb90286f1dc4ce82dd6beba7b5f71b05f5b5
Maycon Douglas <65413165+Maycdouglas@users.noreply.github.com>
2024-05-01 18:17:32 +0000

    Cria tela `em construção` do meu website profissional

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1 style={{ padding: "10px", fontSize: "4rem" }}>EM CONSTRUÇÃO!</h1>
+    </div>
+  );
+}
